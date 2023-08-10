f33b2af054772b8efa273573f282822ec18dc002
rockske8er <rockske8er@gmail.com>
2023-08-10 00:31:52 -0300

    feat: add eslint config

--- src/utils/getNextDays.spec.ts
+++ src/utils/getNextDays.spec.ts
@@ -1,9 +1,9 @@
-import { getNextDays } from "./getNextDays"
+import { getNextDays } from './getNextDays'
 
 describe('getNextDays', () => {
-  it('should be return the next five days',  () => { 
+  it('should be return the next five days', () => {
     const days = getNextDays()
-  
+
     expect(days.length).toBe(5)
   })
 })
